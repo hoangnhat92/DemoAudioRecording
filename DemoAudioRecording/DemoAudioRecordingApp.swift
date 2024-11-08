e180f37669ccea4d2b31673696be5f2c14f43639
nhatdoan1992 <nhat@coderpush.com>
2024-11-08 13:53:00 +0700

    Initial Commit

---
+++ DemoAudioRecording/DemoAudioRecordingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoAudioRecordingApp.swift
+//  DemoAudioRecording
+//
+//  Created by nhat on 8/11/24.
+//
+
+import SwiftUI
+
+@main
+struct DemoAudioRecordingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DemoAudioRecordingTests/DemoAudioRecordingTests.swift
@@ -0,0 +1,17 @@
+//
+//  DemoAudioRecordingTests.swift
+//  DemoAudioRecordingTests
+//
+//  Created by nhat on 8/11/24.
+//
+
+import Testing
+@testable import DemoAudioRecording
+
+struct DemoAudioRecordingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
